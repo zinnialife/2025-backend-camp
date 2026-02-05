6c2e8d65a606dc92bc1e9854e5ba2d03840e7d64
zinnialife <zinnia626868@gmail.com>
2026-02-05 14:59:29 +0800

    初階任務

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,7 @@ app.use(
 );
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/healthcheck", (req, res) => {
+app.get('/healthcheck', (req, res) => {
   res.status(200).json({
     status: "success",
     message: "OK",
